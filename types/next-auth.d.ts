04d02bc7117fccadff5126cabb30b7a1c1179eb3
pogbonna-IAL <pogbonna@gmail.com>
2025-12-26 10:26:20 +0100

    Update Typescript types issues9

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -7,7 +7,14 @@ declare module "next-auth" {
       name?: string | null;
       email?: string | null;
       image?: string | null;
+      role?: string | null;
     };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: string | null;
   }
 }
 
